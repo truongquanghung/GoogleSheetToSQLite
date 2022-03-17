9f9b45ee1e355b616192f92ed3c3c50e9dab2e58
Truong Quang Hung <truongquanghunghn@gmail.com>
2022-03-17 10:23:37 +0700

    Update: Add change from csv and excel file to database

--- convert.py
+++ convert.py
@@ -1,5 +1,5 @@
 # Import required modules
-import csv
+import pandas as pd
 import sqlite3
 import time
 
@@ -7,53 +7,28 @@ start = time.time()
 # Connecting to the database
 connection = sqlite3.connect('db.sqlite3')
 
-# Creating a cursor object to execute SQL queries on a database table
-cursor = connection.cursor()
-
 name = input()
 file_name = input()
 
 # Table Definition
 try:
-    create_table = f'CREATE TABLE {name}(id INTEGER PRIMARY KEY AUTOINCREMENT, '
-    # Opening the .csv file
-    file = open(file_name)
-
-    # Reading the contents of the .csv file
-    contents = csv.reader(file)
-    row1 = next(contents)
-    row_name = ""
-    value = ""
-    for i in range(0, len(row1)-1):
-        tg = str(row1[i]).replace(' ','_')
-        create_table = create_table + "'" + tg + "' TEXT ,"
-        row_name = row_name + "'" + tg + "', "
-        value = value + "?, "
-
-    tg = str(row1[len(row1)-1]).replace(' ','_')
-    create_table = create_table + "'" + tg + "' TEXT);"
-    row_name = row_name + "'" + tg + "'"
-    value = value + "?"
-
-    # Creating the table into our database
-    cursor.execute(create_table)
-
-    # SQL query to insert data into the table
-    insert_records = f"INSERT INTO {name} ({row_name}) VALUES({value})"
-
-    # Importing the contents of the file into our person table
-    cursor.executemany(insert_records, contents)
-
-    # Commiting the changes
-    connection.commit()
-
+    if '.csv' in file_name:
+        # Reading the contents of the .csv file
+        df = pd.read_csv(file_name)
+        df.index += 1
+        df.columns = df.columns.str.replace(' ','_')
+        df.to_sql(name, connection)
+    else:
+        dfs = pd.read_excel(file_name, sheet_name=None)
+        for sheet, df in dfs.items():
+            df.index += 1
+            df.columns = df.columns.str.replace(' ','_')
+            df.to_sql(name, connection)
     # closing the database connection
     connection.close()
 
-except:
-    print('Table exist')
-
-
+except Exception as e:
+    print(e)
 
 end = time.time()
 
--- download_convert.py
+++ download_convert.py
@@ -1,56 +1,20 @@
 # Import required modules
 import pandas as pd
-import csv
 import sqlite3
-from io import StringIO
 
 def make_table(link, name):
     try:
-        # Download data form Google Sheet to csv file 
+        # Download data form Google Sheet 
         segments = link.rpartition('/')
         gid = link.rpartition('gid')
         link = segments[0] + "/export?format=csv&gid" + gid[2]
-        csv_content = pd.read_csv(link)
-        buffer = StringIO()  #creating an empty buffer
-        csv_content.to_csv(buffer, index=False)  #filling that buffer
-        buffer.seek(0)
+        df = pd.read_csv(link)
 
         # Connecting to the database
         connection = sqlite3.connect('db.sqlite3')
-
-        # Creating a cursor object to execute SQL queries on a database table
-        cursor = connection.cursor()
-
-        # Table Definition
-        create_table = f'CREATE TABLE {name}(id INTEGER PRIMARY KEY AUTOINCREMENT, '
-
-        # Opening the .csv file
-        contents = csv.reader(buffer)
-        row1 = next(contents)
-        row_name = ""
-        value = ""
-        for i in range(0, len(row1)-1):
-            tg = str(row1[i]).replace(' ','_')
-            create_table = create_table + "'" + tg + "' TEXT ,"
-            row_name = row_name + "'" + tg + "', "
-            value = value + "?, "
-
-        tg = str(row1[len(row1)-1]).replace(' ','_')
-        create_table = create_table + "'" + tg + "' TEXT);"
-        row_name = row_name + "'" + tg + "'"
-        value = value + "?"
-
-        # Creating the table into our database
-        cursor.execute(create_table)
-
-        # SQL query to insert data into the table
-        insert_records = f"INSERT INTO {name} ({row_name}) VALUES({value})"
-
-        # Importing the contents of the file into our person table
-        cursor.executemany(insert_records, contents)
-
-        # Commiting the changes
-        connection.commit()
+        df.index += 1
+        df.columns = df.columns.str.replace(' ','_')
+        df.to_sql(name, connection)
 
         # closing the database connection
         connection.close()
